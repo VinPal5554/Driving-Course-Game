dbf362de4bc8976fe98e1f384332959e9a132796
Vin <vinaypal5555@gmail.com>
2023-06-18 14:38:30 +0100

    Initial Commit
    The entire project.

---
+++ Unit1Project/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    Vector3 testVector = new Vector3(0, 0, 1);
+    private Vector3 offset = new Vector3(0, 7, -10);
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        // Assign the camera position to vehicle position plus some offset value
+        transform.position = player.transform.position + offset;
+    }
+}
